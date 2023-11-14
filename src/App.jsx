f86a1f983d49be815072f90c5d75c3895789669a
preston176 <prestonnyamweya@gmail.com>
2023-11-14 22:43:34 +0300

    Update App styling and add Stripe payment functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,22 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
+import StripeContainer from './Components/StripeContainer'
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const [showItem, setShowItem] = useState(false)
+
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className='App'>
+      <h1>The Pen Store</h1>
+      {showItem ? <StripeContainer /> : <>
+        <h3>$10.00</h3>
+        <img className='image' src="http://t0.gstatic.com/licensed-image?q=tbn:ANd9GcSYb-8BA6Jj_FoXJK1KVwNzuqAShDFaGVhrvhQwhsY3v9XQExDCtY_sU0lasSH009WmD3yTfvqEds6Xp8vpFcA" alt="image" />
+        <button onClick={() => setShowItem(true)}>Buy Now</button>
+      </>}
+    </div>
   )
 }
 
