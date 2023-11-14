e5b3dc9c0e528e161d07513ab971cacbea00d9be
preston176 <prestonnyamweya@gmail.com>
2023-11-14 21:57:55 +0300

    Add PaymentForm component to StripeContainer

--- src/Components/StripeContainer.jsx
+++ src/Components/StripeContainer.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Elements } from "@stripe/react-stripe-js"
 import { loadStripe } from "@stripe/stripe-js"
+import PaymentForm from './PaymentForm'
 
 const PUBLIC_KEY = "pk_test_51NUHNtEFBkCVrySQmEOwMeISenUaRwqZrIsZxeldYPXExHv6tf6Ai2QQ3GwJD42Aj9E4GETXRC3THsdFC1UeeWXV00gxtdBqY0"
 
@@ -9,6 +10,8 @@ const stripeTestPromise = loadStripe(PUBLIC_KEY)
 
 export default function StripeContainer() {
     return (
-        <div>StripeContainer</div>
+        <Elements stripe={stripeTestPromise}>
+            <PaymentForm />
+        </Elements>
     )
 }
