2d086a2fbfd9d8054ec5636b7605c299f953ba9e
preston176 <prestonnyamweya@gmail.com>
2023-11-14 22:07:14 +0300

    Add Stripe payment functionality to PaymentForm component

--- src/Components/PaymentForm.jsx
+++ src/Components/PaymentForm.jsx
@@ -1,8 +1,44 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { CardElement, useElements, useStripe } from "@stripe/react-stripe-js"
+import axios from "axios"
 
 function PaymentForm() {
+    const [success, setSuccess] = useState(false)
+    const stripe = useStripe()
+    const elements = useElements();
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        const { error, paymentMethod } = await stripe.createPaymentMethod({
+            type: "card",
+            card: elements.getElement(CardElement)
+        })
+
+
+        if (!error) {
+            try {
+                const { id } = paymentMethod
+                const response = await axios.post("http://localhost:4000/payments", {
+                    amount: 1000,
+                    id
+                })
+
+                if (response.data.success) {
+                    console.log("Successful payment")
+                    setSuccess(true)
+                }
+            }
+            catch (error) {
+                console.log("Error", error)
+            }
+        } else {
+            console.log(error.message)
+        }
+    }
     return (
-        <div>PaymentForm</div>
+        <div>
+
+        </div>
     )
 }
 
